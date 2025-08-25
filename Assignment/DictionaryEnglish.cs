764f6e939cf7c8999046a4a4aecafe569bb074c5
Mena Erian <mena3koko2karas1@gmail.com>
2025-08-25 17:15:04 +0300

    LINQ - Aggregate Operators From 1 to 9

---
+++ Assignment/DictionaryEnglish.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Assignment
+{
+    internal static class DictionaryEnglish
+    {
+        public static string[] Line { get; set; }
+
+        static DictionaryEnglish()
+        {
+            //Line = File.ReadAllText("dictionary_english.txt");
+            Line = File.ReadAllLines("dictionary_english.txt");
+        }
+
+    }
+
+}
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace Assignment
+﻿using Demo;
+using static Demo.ListGenerator;
+
+
+namespace Assignment
 {
     internal class Program
     {
@@ -35,6 +39,71 @@
 
             //Result.Print();
             #endregion
+
+            #region LINQ - Aggregate Operators
+
+            #region 1. Uses Count to get the number of odd numbers in the array
+            /// int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            /// Arr.Count(num => num % 2 != 0).Print();
+            #endregion
+
+            #region 2. Return a list of customers and how many orders each has.
+            /// var Result = CustomersList.Select(ctm => new
+            /// {
+            ///     ctm.CustomerName,
+            ///     OrderCount = ctm.Orders.Length,
+            ///     //OrderCount = ctm.Orders.Count(),
+            /// });
+            /// Result.PrintAll();
+            #endregion
+
+            #region 3. Return a list of categories and how many products each has
+            /// var Result = ProductsList.GroupBy(p => p.Category);
+            /// Result.PrintAll();
+            #endregion
+
+            #region 4. Get the total of the numbers in an array.
+            /// int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            /// Arr.Sum().Print();
+            #endregion
+
+            #region 5. Get the total number of characters of all words in dictionary_english.txt 
+            /// var Result = DictionaryEnglish.Line.Sum(str => str.Length);
+            /// Result.Print();
+            #endregion
+
+            #region 6. Get the length of the shortest word in dictionary_english.txt (Read dictionary_english.txt into Array of String First).
+            /// //var smallestElement = DictionaryEnglish.Line.MinBy(str => str.Length);
+            /// //smallestElement.Print();
+            /// 
+            /// var Result = DictionaryEnglish.Line.Min(str => str.Length);
+            /// Result.Print();
+            #endregion
+
+            #region 7. Get the length of the longest word in dictionary_english.txt (Read dictionary_english.txt into Array of String First).
+            /// var Result = DictionaryEnglish.Line.Max(str => str.Length);
+            /// Result.Print();
+            #endregion
+
+            #region 8. Get the average length of the words in dictionary_english.txt (Read dictionary_english.txt into Array of String First).
+            /// var Result = DictionaryEnglish.Line.Average(str => str.Length);
+            /// Result.Print();
+            #endregion
+
+            #region 9. Get the total units in stock for each product category.
+            /// var Result = ProductsList.GroupBy(p => p.Category, (key, products) => new
+            /// {
+            ///     key,
+            ///     UntisInStock = products.Sum(p => p.UnitsInStock)
+            /// });
+            /// 
+            /// Result.PrintAll();
+            #endregion
+
+
+
+            #endregion
+
         }
     }
 }
